015b6a2ae2bfa664dcfc91fa5d396676418a4824
golddick <63362187+golddick@users.noreply.github.com>
2025-01-13 06:38:56 +0100

    ss

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,8 +61,8 @@ $(function () {
 			$this.html(event.strftime('<div class="countdown d-flex"><div class="single-count-content"><span class="count">%D</span><p class="text">Days</p></div><div class="single-count-content"><span class="count">%H</span><p class="text">Hours</p></div><div class="single-count-content"><span class="count">%M</span><p class="text">Minutes</p></div><div class="single-count-content"><span class="count">%S</span><p class="text">Seconds</p></div></div>'));
 		});
 	});
-
-	// WOW active
+  
+	// WOW activee
 	new WOW().init();
 
 });	
